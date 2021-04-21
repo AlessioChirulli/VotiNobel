b66a52d752b513d5689d6adfaa2f2325a152f226
alessiochirulli <s258303@studenti.polito.it>
2021-04-21 09:31:51 +0200

    Prova di miglioramento prestazioni

--- src/main/java/it/polito/tdp/nobel/model/Model.java
+++ src/main/java/it/polito/tdp/nobel/model/Model.java
@@ -1,16 +1,118 @@
 package it.polito.tdp.nobel.model;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
+import it.polito.tdp.nobel.db.EsameDAO;
+
 public class Model {
+	private List<Esame> partenza;
+	private Set<Esame>soluzioneMigliore;
+	private double mediaSoluzioneMigliore;
+	
+	public Model() {
+		EsameDAO dao= new EsameDAO();
+		this.partenza = dao.getTuttiEsami();
+		
+	}
+	
 
 	
 	public Set<Esame> calcolaSottoinsiemeEsami(int numeroCrediti) {
-		System.out.println("TODO!");
-		return null;	
+		Set<Esame> parziale = new HashSet<Esame>();
+		soluzioneMigliore = new HashSet<Esame>();
+		mediaSoluzioneMigliore=0;
+		
+		cerca1(parziale,0,numeroCrediti);
+		//cerca1(parziale,0,numeroCrediti);
+		
+		return soluzioneMigliore;	
 	}
 
-	
+	/* COMPLESSITA' 2^N */
+	private void cerca2(Set<Esame> parziale, int L, int m) {
+		// casi terminali
+		
+				 int crediti = sommaCrediti(parziale);
+				 
+				 if(crediti > m) {
+					 return ;
+				 }
+				 
+				 if(crediti == m) {
+					double media = calcolaMedia(parziale);
+					if (media > mediaSoluzioneMigliore) {
+						soluzioneMigliore = new HashSet<>(parziale);
+						mediaSoluzioneMigliore = media;
+					}
+					return ;
+				 }
+				 
+				 //sicuramente crediti < m
+				// 1) L= N -> non ci sono più esami da aggiungere
+				if(L == partenza.size()) {
+					return ;
+				}
+				
+				//generare i sotto-problemi
+				// partenza[L] è da aggiungere oppure no? Provo entrambe le cose
+				parziale.add(partenza.get(L));
+				cerca2(parziale,L+1,m);
+				
+				parziale.remove(partenza.get(L));
+				cerca2(parziale,L+1,m);
+		
+	}
+
+
+	/* COMPLESSITA' : N! */
+	private void cerca1(Set<Esame> parziale, int L, int m) {
+		// casi terminali
+		
+		 int crediti = sommaCrediti(parziale);
+		 
+		 if(crediti > m) {
+			 return ;
+		 }
+		 
+		 if(crediti == m) {
+			double media = calcolaMedia(parziale);
+			if (media > mediaSoluzioneMigliore) {
+				soluzioneMigliore = new HashSet<>(parziale);
+				mediaSoluzioneMigliore = media;
+			}
+			return ;
+		 }
+		 
+		 //sicuramente crediti < m
+		// 1) L= N -> non ci sono più esami da aggiungere
+		if(L == partenza.size()) {
+			return ;
+		}
+		
+		
+		//generare i sotto-problemi
+		for (int i=L;i<partenza.size();i++) {
+			if(!parziale.contains(partenza.get(i))) {
+				parziale.add(partenza.get(i));
+				cerca1(parziale, L+1,m);
+				parziale.remove(partenza.get(i));
+			}
+		}
+		/*
+		for(Esame e : partenza) {
+			if(!parziale.contains(e)) {
+				parziale.add(e);
+				cerca1(parziale, L+1, m);
+				parziale.remove(e);
+			}
+		}
+		*/
+	}
+
+
+
 	public double calcolaMedia(Set<Esame> esami) {
 		
 		int crediti = 0;
